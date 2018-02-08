903310672581afe4d3338f8857c425df551000bf
vaibhav <jain0031@algonquinlive.com>
2018-02-08 11:21:22 -0500

    lab 3

--- app/src/main/java/com/example/vaibhavjain/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/vaibhavjain/androidlabs/MainActivity.java
@@ -1,14 +1,88 @@
 package com.example.vaibhavjain.androidlabs;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+    protected static final String ACTIVITY_NAME = "StartActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(ACTIVITY_NAME,"IN onCreate(): ");
+        Button button2 = (Button)findViewById(R.id.button);
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent secondIntent = new Intent(MainActivity.this,
+                        ListitemsActivity.class);
+                startActivityForResult(secondIntent, 50);
+            }
+        });
+    }
+
+    public void onActivityResult( int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == 50)
+        {
+
+            Log.i("MainActivity", "Returned to StartActivity.onActivityResult");
+        }
+        else if (resultCode== Activity.RESULT_OK){
+            String messagePassed = data.getStringExtra("Response");
+            Toast toast = Toast.makeText(MainActivity.this , messagePassed,Toast.LENGTH_LONG); //this is the ListActivity
+            toast.show(); //display your message box
+
+        }
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "in onResume() : ");
+
+    }
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart() : ");
+
+
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause() : ");
+
+
+    }
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, " onStop() : ");
+
+
+    }
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, " onDestroy() : ");
+
+
     }
 }
+
 
